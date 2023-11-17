b92b886216a2d92a9921e05587b9d4fe0c295914
terry.yes <terry.yes@kakaoent.com>
2023-11-17 09:43:55 +0900

    02solution_snapkit 답변 추가

--- 01UIKit/000autolayout_basic/02solution_snapkit/02solution_snapkit/ViewController.swift
+++ 01UIKit/000autolayout_basic/02solution_snapkit/02solution_snapkit/ViewController.swift
@@ -6,14 +6,30 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        let myView = UIView()
+        myView.backgroundColor = .red
+        myView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(myView)
+
+        myView.snp.makeConstraints { (make) in
+            make.centerX.equalToSuperview()
+            make.centerY.equalToSuperview()
+            make.width.equalTo(100)
+            make.height.equalTo(100)
+        }
+
+        // 간단히 아래와 같이 쓸 수도 있습니다.
+        // myView.snp.makeConstraints { (make) in
+        //     make.center.equalToSuperview()
+        //     make.width.height.equalTo(100)
+        // }
     }
-
-
 }
 
