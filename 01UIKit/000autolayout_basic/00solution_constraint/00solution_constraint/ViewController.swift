82633dbf863074fb83e1f1835d487f436d246dfc
terry.yes <terry.yes@kakaoent.com>
2023-11-17 09:27:01 +0900

    autoylayout_basic 00solution 답추가

--- 01UIKit/000autolayout_basic/00solution_constraint/00solution_constraint/ViewController.swift
+++ 01UIKit/000autolayout_basic/00solution_constraint/00solution_constraint/ViewController.swift
@@ -11,9 +11,15 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        let myView = UIView()
+        myView.backgroundColor = .red
+        myView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(myView)
+        myView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        myView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        myView.widthAnchor.constraint(equalToConstant: 100).isActive = true
+        myView.heightAnchor.constraint(equalToConstant: 100).isActive = true
     }
-
-
 }
 
